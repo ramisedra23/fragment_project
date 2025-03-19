185e3741d413a2b76579ee23a86705edcb96f0c5
rami <ramisedra@gmail.com>
2025-03-19 10:37:37 +0300

    adding a back button in show more fragment to return back to profile fragment

--- app/src/main/java/com/example/cvproject/ShowMore.kt
+++ app/src/main/java/com/example/cvproject/ShowMore.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class MoreInfoFragment : Fragment(R.layout.fragment_showmore) {
 
@@ -20,9 +21,13 @@ class MoreInfoFragment : Fragment(R.layout.fragment_showmore) {
         val view = inflater.inflate(R.layout.fragment_showmore, container, false)
 
         val btnLinkedIn: Button = view.findViewById(R.id.open_linkedin)
+        val btnBack: Button = view.findViewById(R.id.btnback)
         btnLinkedIn.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://jo.linkedin.com/in/rami-raed-02b0b7225"))
             startActivity(intent)
+        }
+        btnBack?.setOnClickListener {
+            findNavController().navigateUp()
         }
 
         return view
